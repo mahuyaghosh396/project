77c143f0f806f2c7210bed03121f0a65bc81dc98
ankita12345678910 <96244331+ankita12345678910@users.noreply.github.com>
2022-05-22 14:13:37 +0530

    login & registration page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -28,4 +28,26 @@ class HomeController extends AbstractController
         return $this->render('home/signup.html.twig', [
         ]);
     }
+
+    #[Route('/view/notice', name: 'web_all_notice')]
+    public function viewNotice(): Response
+    {
+        return $this->render('home/notice.html.twig', [
+            'title'=>"notice"
+        ]);
+    }
+    #[Route('/add/notice', name: 'web_add_notice')]
+    public function addNotice(): Response
+    {
+        return $this->render('home/add-notice.html.twig', [
+            'title'=>"notice"
+        ]);
+    }
+    #[Route('/contact', name: 'web_contact')]
+    public function contact(): Response
+    {
+        return $this->render('home/contact.html.twig', [
+            'title'=>"contact"
+        ]);
+    }
 }
