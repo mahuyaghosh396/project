6c690142fe61f598dba2773918406b82b9bbaf5f
ankita12345678910 <96244331+ankita12345678910@users.noreply.github.com>
2022-05-24 23:32:12 +0530

    contact type

---
+++ src/Form/ContactType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name',TextType::class,['attr' => ['class' => 'form-control']])
+            ->add('email',EmailType::class,['attr' => ['class' => 'form-control']])
+            ->add('message',TextareaType::class,['attr' => ['class' => 'form-control']])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+        ]);
+    }
+}
