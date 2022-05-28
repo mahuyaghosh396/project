b2df3c212f3f06c5b61c19ae9315fdb4900009df
ankita12345678910 <96244331+ankita12345678910@users.noreply.github.com>
2022-05-28 18:47:04 +0530

    about us

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,10 +8,14 @@ use App\Form\ContactType;
 use App\Form\NoticeType;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
-
+use Symfony\Component\HttpKernel\Exception\UnsupportedMediaTypeHttpException;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class HomeController extends AbstractController
 {
@@ -19,6 +23,13 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
+        ]);
+    }
+
+    #[Route('/front', name: 'web_frontpage')]
+    public function home(): Response
+    {
+        return $this->render('home/home.html.twig', [
         ]);
     }
 
@@ -39,13 +50,43 @@ class HomeController extends AbstractController
     #[Route('/view/notice', name: 'web_all_notice')]
     public function viewNotice(ManagerRegistry $doctrine): Response
     {
+       
         $notices = $doctrine->getRepository(Notice::class)->findAll();
+        $em = $doctrine->getManager();
+        $today = new \DateTime();
+        $query = $em->createQuery("SELECT u.name from App:Notice u where u.noticeTo < :today");
+        $query->setParameter('today', $today);
+        //dd($query->getResult());
+        
+        
+        if($query){
+        // $fs=new Filesystem();
+
+        // // $notice = $media->getNotice();
+        // // $em->remove($media);
+        // $em->flush();
+
+        // return $this->redirectToRoute('web_add_notice');
+    
+
+        //dd($path='%kernel.project_dir%/public/uploads/notices'.$query);
+        // $fs->remove($path);
+        // $em->flush();
+
+           // unlink('upload_directory').'/uploads/notices/';
+       // $fs->remove('/uploads/notices/'.$query->getResult());
+        // dd($fs);
+        // $em->flush();
+        // dd("123");
+        }
+       
+        
         return $this->render('home/notice.html.twig', [
             "notices" => $notices
         ]);
     }
     #[Route('/admin/add/notice', name: 'web_add_notice')]
-    public function addNotice(Request $request, ManagerRegistry $doctrine): Response
+    public function addNotice(Request $request, ManagerRegistry $doctrine,SluggerInterface $slugger): Response
 
     {
 
@@ -55,6 +96,38 @@ class HomeController extends AbstractController
         if ($request->getMethod() == "POST") {
             
             if ($form->isSubmitted() and $form->isValid()) {
+
+                /** @var UploadedFile $upload */
+                $upload = $form->get('file')->getData();
+
+                if ($upload) {
+
+                    /*original file name..if my file name is "my_first_notice"..then $originalfilename
+                    return the actual name of the file*/
+                    $originalFilename = pathinfo($upload->getClientOriginalName(), PATHINFO_FILENAME);
+
+                    //$filename returns the file name like that " my-first-notice " 
+                    $Filename = $slugger->slug($originalFilename);
+
+                    //$newFilename returns a unique file name with extension..like that" my-first-notice.pdf"
+                    //guessExtension()is a method which returns the original extension of the file.
+                    $newFilename = $Filename . '-' . uniqid() . '.' . $upload->guessExtension();
+
+                    //move uploaded file....
+                    try {
+                        $upload->move(
+                            $this->getParameter('upload_directory'),
+                            $newFilename
+                        );
+                    } catch (FileException $e) {
+                        // ... handle exception if something happens during file upload
+                    }
+
+
+                    //set file name....with it's new file name....
+                    $notice->setFile($newFilename);
+                  
+
                 
                
                 $em = $doctrine->getManager();
@@ -70,6 +143,7 @@ class HomeController extends AbstractController
                 return $this->redirect($this->generateUrl('web_add_notice'));
             }
         }
+    }
 
         return $this->render('home/add-notice.html.twig', [
             'title' => "Add Notice",
@@ -104,4 +178,5 @@ class HomeController extends AbstractController
         ]);
     }
     }
+
 
