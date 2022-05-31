74d2850c9fe19b7e50cd0705138999cd7781e9ee
ankita12345678910 <96244331+ankita12345678910@users.noreply.github.com>
2022-05-31 16:07:52 +0530

    updated notice list..manage notice..

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,14 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
 use App\Entity\Notice;
 use App\Entity\User;
+use App\Form\ContactType;
+use App\Form\NoticeType;
+use App\Form\UserType;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class AdminController extends AbstractController
 {
@@ -21,16 +28,34 @@ class AdminController extends AbstractController
         ]);
     }
 
+    #[Route('/update/notice/{id}', name: 'app_update_notice')]
+    public function updateNotice(ManagerRegistry $doctrine, $id): Response
+    {
+
+        $em = $doctrine->getManager();
+        $notice = $doctrine->getRepository("App\Entity\Notice")->findOneBy(["id" => $id]);
+        if ($notice->getStatus() == "Active") {
+            $notice->setStatus("Deleted");
+            $em->persist($notice);
+            $em->flush();
+        } else {
+            $notice->setStatus("Active");
+            $em->persist($notice);
+            $em->flush();
+        }
+        return $this->redirect($this->generateUrl('web_all_notice'));
+    }
+
     #[Route('admin/add/user', name: 'app_admin_add_user')]
     public function addUser(ManagerRegistry $doctrine, UserPasswordHasherInterface $passwordHasher): Response
     {
         $em = $doctrine->getManager();
         $user = new User();
-        $user->setFirstName('dev');
-        $user->setLastName('admin');
-        $user->setEmail('admin@g.c');
+        $user->setFirstName('Ankita');
+        $user->setLastName('Baidya');
+        $user->setEmail('ankita@g.c');
         $user->setCellphone('12345');
-        $user->setRoles(['ROLE_ADMIN']);
+        $user->setRoles(['ROLE_STUDENT']);
         $user->setPassword($passwordHasher->hashPassword($user, "1234"));
         $em->persist($user);
         $em->flush();
@@ -39,23 +64,6 @@ class AdminController extends AbstractController
         ]);
     }
 
-    #[Route('/admin/manage/notice', name: 'app_admin_manage_notice')]
-    public function manageNotice(Request $request, ManagerRegistry $doctrine): Response
-    {
-        $em = $doctrine->getManager();
-
-        if ($request->getMethod() == "POST") {
-            $notice = new Notice();
-            $notice->setName($request->get('name'));
-            $notice->setNoticeFrom(new \DateTime($request->get('notice_from')));
-            $notice->setNoticeTo(new \DateTime($request->get('notice_to')));
-
-            $em->persist($notice);
-            $em->flush();
-        }
-
-        return $this->render('admin/manage_notice.html.twig', []);
-    }
 
     #[Route('/admin/list/notice', name: 'app_admin_list_notice')]
     public function listNotice(ManagerRegistry $doctrine): Response
@@ -67,9 +75,10 @@ class AdminController extends AbstractController
     }
 
     #[Route('/admin/download/{path}/{file}', name: 'app_admin_download')]
-    public function download($path, $file): Response
+    public function download($path, $file)
     {
-        $filename = $path."/".$file;
+        $filename = $this->getParameter('upload_directory') . "/" . $path . "/" . $file;
+
         // Generate response
         $response = new Response();
 
@@ -81,6 +90,144 @@ class AdminController extends AbstractController
         // Send headers before outputting anything
         $response->sendHeaders();
         $response->setContent(file_get_contents($filename));
-        return $response;
+        return 0;
+    }
+
+    #[Route('/view/notice', name: 'web_all_notice')]
+    public function viewNotice(ManagerRegistry $doctrine): Response
+    {
+
+        $em = $doctrine->getManager();
+
+        // $abc=$this->getUser();
+        // $query = $em->createQuery("SELECT u.roles from App:User u where u= :abc");
+        // $query->setParameter('abc', $abc);
+        // $a=$query->getResult();
+        // dump($a);
+
+        if ($this->isGranted('ROLE_ADMIN')) {
+
+            $query = $em->createQuery("SELECT u from App:Notice u");
+            $query->getResult();
+        } else {
+            $today = new \DateTime();
+            $query = $em->createQuery("SELECT u from App:Notice u where u.noticeTo > :today and u.status ='active'");
+            $query->setParameter('today', $today);
+        }
+
+        return $this->render('admin/list_notice.html.twig', [
+            "notices" => $query->getResult()
+        ]);
+    }
+
+    #[Route('/current/notice', name: 'web_current_notice')]
+    public function currentNotice(ManagerRegistry $doctrine): Response
+    {
+
+        $em = $doctrine->getManager();
+        $yesterday = new \DateTime("yesterday");
+        $query = $em->createQuery("SELECT u from App:Notice u where u.noticeFrom > :yesterday");
+        $query->setParameter('yesterday', $yesterday);
+
+        return $this->render('admin/current_notice.html.twig', [
+            "notices" => $query->getResult()
+        ]);
+    }
+
+    #[Route('/admin/manage/notice', name: 'web_manage_notice')]
+    public function manageNotice(Request $request, ManagerRegistry $doctrine, SluggerInterface $slugger): Response
+
+    {
+
+        $notice = new Notice();
+        $form = $this->createForm(NoticeType::class, $notice);
+        $form->handleRequest($request);
+        if ($request->getMethod() == "POST") {
+
+            if ($form->isSubmitted() and $form->isValid()) {
+
+                /** @var UploadedFile $upload */
+                $upload = $form->get('file')->getData();
+
+                if ($upload) {
+
+                    /*original file name..if my file name is "my_first_notice"..then $originalfilename
+                    return the actual name of the file*/
+                    $originalFilename = pathinfo($upload->getClientOriginalName(), PATHINFO_FILENAME);
+
+                    //$filename returns the file name like that " my-first-notice " 
+                    $Filename = $slugger->slug($originalFilename);
+
+                    //$newFilename returns a unique file name with extension..like that" my-first-notice.pdf"
+                    //guessExtension()is a method which returns the original extension of the file.
+                    $newFilename = $Filename . '-' . uniqid() . '.' . $upload->guessExtension();
+
+                    //move uploaded file....
+                    try {
+                        $upload->move(
+                            $this->getParameter('upload_directory') . "/notices/",
+                            $newFilename
+                        );
+                    } catch (FileException $e) {
+                        // ... handle exception if something happens during file upload
+                    }
+
+
+                    //set file name....with it's new file name....
+                    $notice->setFile($newFilename);
+
+
+
+
+                    $em = $doctrine->getManager();
+
+                    $em->persist($notice);
+
+                    $em->flush();
+
+                    $request->getSession()->getFlashBag()->add("successmsg", "Notice uploaded Successfully");
+                    return $this->redirect($this->generateUrl('web_add_notice'));
+                } else {
+                    $request->getSession()->getFlashBag()->add("errormsg", "something went wrong!!");
+                    return $this->redirect($this->generateUrl('web_add_notice'));
+                }
+            }
+        }
+
+        return $this->render('admin/manage_notice.html.twig', [
+            'title' => "Add Notice",
+            'form' => $form->createView()
+        ]);
+    }
+    #[Route('/contact', name: 'web_contact')]
+    public function contact(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+        if ($request->getMethod() == "POST") {
+
+            if ($form->isSubmitted() and $form->isValid()) {
+
+                $em = $doctrine->getManager();
+                $em->persist($contact);
+                $em->flush();
+                $request->getSession()->getFlashBag()->add("successmsg", "Your message has been received successfully");
+                return $this->redirect($this->generateUrl('web_contact'));
+            } else {
+                $request->getSession()->getFlashBag()->add("errormsg", "something went wrong!!");
+                return $this->redirect($this->generateUrl('web_contact'));
+            }
+        }
+
+        return $this->render('admin/contact.html.twig', [
+            'title' => "Contact",
+            'form' => $form->createView()
+        ]);
+    }
+    #[Route('/signup', name: 'web_signup')]
+    public function signup(): Response
+    {
+        return $this->render('admin/signup.html.twig', []);
     }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,21 +1,10 @@
 <?php
 
 namespace App\Controller;
-
-use App\Entity\Contact;
-use App\Entity\Notice;
-use App\Form\ContactType;
-use App\Form\NoticeType;
-use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Exception\UnsupportedMediaTypeHttpException;
-use Symfony\Component\String\Slugger\SluggerInterface;
+
 
 class HomeController extends AbstractController
 {
@@ -30,146 +19,9 @@ class HomeController extends AbstractController
     {
         return $this->render('home/home.html.twig', []);
     }
-
     #[Route('/about', name: 'web_about_us')]
     public function aboutUs(): Response
     {
         return $this->render('home/about_us.html.twig', []);
-    }
-
-    #[Route('/signup', name: 'web_signup')]
-    public function signup(): Response
-    {
-        return $this->render('home/signup.html.twig', []);
-    }
-
-    #[Route('/view/notice', name: 'web_all_notice')]
-    public function viewNotice(ManagerRegistry $doctrine): Response
-    {
-
-        $em = $doctrine->getManager();
-        $today = new \DateTime();
-        $query = $em->createQuery("SELECT u from App:Notice u where u.noticeTo > :today");
-        $query->setParameter('today', $today);
-
-        return $this->render('home/notice.html.twig', [
-            "notices" => $query->getResult()
-        ]);
-    }
-
-    #[Route('/current/notice', name: 'web_current_notice')]
-    public function currentNotice(ManagerRegistry $doctrine): Response
-    {
-
-        $em = $doctrine->getManager();
-        $yesterday = new \DateTime("yesterday");
-        $query = $em->createQuery("SELECT u from App:Notice u where u.noticeFrom > :yesterday");
-        $query->setParameter('yesterday', $yesterday);
-
-        return $this->render('home/current_notice.html.twig', [
-            "notices" => $query->getResult()
-        ]);
-    }
-
-    #[Route('/admin/all/notice', name: 'web_view_notice')]
-    public function allNotice(ManagerRegistry $doctrine): Response
-    {
-
-        $notices = $doctrine->getRepository(Notice::class)->findAll();
-        return $this->render('home/view_notice.html.twig', [
-            "notices" => $notices
-        ]);
-    }
-    #[Route('/admin/add/notice', name: 'web_add_notice')]
-    public function addNotice(Request $request, ManagerRegistry $doctrine, SluggerInterface $slugger): Response
-
-    {
-
-        $notice = new Notice();
-        $form = $this->createForm(NoticeType::class, $notice);
-        $form->handleRequest($request);
-        if ($request->getMethod() == "POST") {
-
-            if ($form->isSubmitted() and $form->isValid()) {
-
-                /** @var UploadedFile $upload */
-                $upload = $form->get('file')->getData();
-
-                if ($upload) {
-
-                    /*original file name..if my file name is "my_first_notice"..then $originalfilename
-                    return the actual name of the file*/
-                    $originalFilename = pathinfo($upload->getClientOriginalName(), PATHINFO_FILENAME);
-
-                    //$filename returns the file name like that " my-first-notice " 
-                    $Filename = $slugger->slug($originalFilename);
-
-                    //$newFilename returns a unique file name with extension..like that" my-first-notice.pdf"
-                    //guessExtension()is a method which returns the original extension of the file.
-                    $newFilename = $Filename . '-' . uniqid() . '.' . $upload->guessExtension();
-
-                    //move uploaded file....
-                    try {
-                        $upload->move(
-                            $this->getParameter('upload_directory'),
-                            $newFilename
-                        );
-                    } catch (FileException $e) {
-                        // ... handle exception if something happens during file upload
-                    }
-
-
-                    //set file name....with it's new file name....
-                    $notice->setFile($newFilename);
-
-
-
-
-                    $em = $doctrine->getManager();
-
-                    $em->persist($notice);
-
-                    $em->flush();
-
-                    $request->getSession()->getFlashBag()->add("successmsg", "Notice uploaded Successfully");
-                    return $this->redirect($this->generateUrl('web_add_notice'));
-                } else {
-                    $request->getSession()->getFlashBag()->add("errormsg", "something went wrong!!");
-                    return $this->redirect($this->generateUrl('web_add_notice'));
-                }
-            }
-        }
-
-        return $this->render('home/add-notice.html.twig', [
-            'title' => "Add Notice",
-            'form' => $form->createView()
-        ]);
-    }
-    #[Route('/contact', name: 'web_contact')]
-    public function contact(Request $request, ManagerRegistry $doctrine): Response
-    {
-        $contact = new Contact();
-        $form = $this->createForm(ContactType::class, $contact);
-        $form->handleRequest($request);
-        if ($request->getMethod() == "POST") {
-
-            if ($form->isSubmitted() and $form->isValid()) {
-
-
-                $em = $doctrine->getManager();
-                $em->persist($contact);
-                $em->flush();
-                $request->getSession()->getFlashBag()->add("successmsg", "Your message has been received successfully");
-                return $this->redirect($this->generateUrl('web_contact'));
-            } else {
-                $request->getSession()->getFlashBag()->add("errormsg", "something went wrong!!");
-                return $this->redirect($this->generateUrl('web_contact'));
-            }
-        }
-
-        return $this->render('home/contact.html.twig', [
-            'title' => "Contact",
-            'form' => $form->createView()
-        ]);
     }
 }
