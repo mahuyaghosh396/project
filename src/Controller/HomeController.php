9bee3fb6f89c37584f388f454fee49255c1cbd09
ankita12345678910 <96244331+ankita12345678910@users.noreply.github.com>
2022-06-20 23:10:37 +0530

    redirect action bug

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -26,21 +26,21 @@ class HomeController extends AbstractController
         ]);
     }
 
-    #[Route('/redirect', name: 'web_redirect')]
-    public function checkRedirect(Request $request): Response
-    {
-        $url = 'web_homepage';
-        if ($this->isGranted('ROLE_ADMIN')) {
-            $url = 'admin_dashboard';
-        } elseif ($this->isGranted('ROLE_STUDENT')) {
-            $url = 'student_dashboard';
-        } elseif ($this->isGranted('ROLE_LECTURER')) {
-            $url = 'lecturer_dashboard';
-        } else {
-        }
+    // #[Route('/redirect', name: 'web_redirect')]
+    // public function checkRedirect(Request $request): Response
+    // {
+    //     $url = 'web_homepage';
+    //     if ($this->isGranted('ROLE_ADMIN')) {
+    //         $url = 'admin_dashboard';
+    //     } elseif ($this->isGranted('ROLE_STUDENT')) {
+    //         $url = 'student_dashboard';
+    //     } elseif ($this->isGranted('ROLE_LECTURER')) {
+    //         $url = 'lecturer_dashboard';
+    //     } else {
+    //     }
 
-        return $this->redirect($this->generateUrl($url));
-    }
+    //     return $this->redirect($this->generateUrl($url));
+    // }
     
     #[Route('/about/college', name: 'web_college')]
     public function aboutCollege(): Response
