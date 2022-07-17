82633c9f212db3b8af2a4c467ce254b586782f5a
ankita12345678910 <96244331+ankita12345678910@users.noreply.github.com>
2022-07-17 14:30:13 +0530

    updated placeholder in department

--- src/Form/DepartmentType.php
+++ src/Form/DepartmentType.php
@@ -5,20 +5,20 @@ namespace App\Form;
 use App\Entity\Department;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType as TypeTextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class DepartmentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name',TypeTextType::class, [
-                'attr' => ['class' => 'form-control', 'placeholder' => 'name']
+            ->add('name',TextType::class, [
+                'attr' => ['class' => 'form-control', 'placeholder' => 'Department name']
             ])
-            ->add('code',TypeTextType::class, [
-                'attr' => ['class' => 'form-control', 'placeholder' => 'code']
+            ->add('code',TextType::class, [
+                'attr' => ['class' => 'form-control', 'placeholder' => 'Department code']
             ])
             //->add('status')
         ;
